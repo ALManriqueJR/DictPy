a5c2ab0811b66d9e44af838674207a29163dfab1
ALManriqueJR <alessandromanriquejunior@gmail.com>
2022-02-22 17:29:42 -0300

    Most and Least emails sender authors

---
+++ histogram_email.py
@@ -0,0 +1,19 @@
+"""
+Using docstring to REF -> Dr. Charles R. Severance P4E Exercises
+"""
+VAL = 0
+emailHistogram = {}
+# Default encoding é UTF-8
+with open(r'Email Histogram\mbox-short.txt', 'r', encoding='utf-8') as handle:
+    for lines in handle:
+        if lines.startswith('From '):
+            words = lines.split()
+            email = words[1]
+            emailHistogram[email] = emailHistogram.get(email, 0) + 1
+            VAL = list(emailHistogram.values())
+    # print(emailHistogram)
+    a = max(VAL)
+    print(list(emailHistogram.keys())[
+          list(emailHistogram.values()).index(a)], a)
+
+# Consegui fazer o exercicio pg 130 P4E
